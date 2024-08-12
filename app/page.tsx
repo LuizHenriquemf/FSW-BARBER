d6b1f648f97774b5088119b5bff540ac2db45628
LuizHenriquemf <luizhenriquefurquim@hotmail.com>
2024-08-12 17:42:30 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search";
 import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./_lib/auth";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 
 const Home = async () => {
@@ -43,8 +45,17 @@ const Home = async () => {
       {/* header */}
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Luiz</h2>
-        <p>Segunda-feira, 05 de agosto</p>
+        <h2 className="text-xl font-bold">Olá, {session?.user ? session.user.name : " bem vindo!"}</h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR, })}
+          </span>
+          <span>&nbsp;de&nbsp;</span>
+          <span>
+            {format(new Date(), "MMMM", { locale: ptBR, })}
+          </span>
+
+        </p>
 
         {/* BUSCA */}
         <div className="mt-6">
